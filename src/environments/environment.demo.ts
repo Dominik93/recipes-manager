ab8871f54af7b7c37b031a9336efcde2591b5103
Dominik Ślusarz <slusarz.dominik@gmail.com>
2023-12-22 18:46:17 +0100

    add demo

---
+++ src/environments/environment.demo.ts
@@ -0,0 +1,10 @@
+import { MockAuthorizationService } from "../app/services/authorization/mock-authorization.service";
+import { DisabledLoggingService } from "../app/services/logging/disabled-logging.service";
+import { InMemoryRecipesService } from "../app/services/recipes/in-memory-recipes.service";
+
+export const environment = {
+    debug: false,
+    loggingService: DisabledLoggingService,
+    authorizationService: MockAuthorizationService,
+    recipesService: InMemoryRecipesService
+};
