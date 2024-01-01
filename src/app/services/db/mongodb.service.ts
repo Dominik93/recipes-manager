3b7e04f588eb1ea05328d5d954a5c4686fea6f7f
Dominik Ślusarz <slusarz.dominik@gmail.com>
2024-01-01 15:44:22 +0100

    fix token name in rest call

--- src/app/services/db/mongodb.service.ts
+++ src/app/services/db/mongodb.service.ts
@@ -15,39 +15,39 @@ export class MongodbService {
 
   constructor(private http: HttpClient) { }
 
-  findOneDocument(url: string, token: string, name: string) {
+  findOneDocument(url: string, token: string, applicationToken: string) {
     const headers = this.authHeader(token)
       .set('Content-Type', 'application/json')
       .set('Access-Control-Request-Headers', '*');
     return this.http.post(url,
       {
         ...this.connection(),
-        "filter": { "name": name },
+        "filter": { "token": applicationToken },
         "projection": { "recipes": 1, "version": 1 }
       },
       { headers: headers });
   }
 
-  insertOneDocument(url: string, token: string, name: string, version: number, documents: Recipe[]) {
+  insertOneDocument(url: string, token: string, applicationToken: string, version: number, documents: Recipe[]) {
     const headers = this.authHeader(token)
       .set('Content-Type', 'application/ejson')
       .set('Accept', 'application/json');
     return this.http.post(url,
       {
         ...this.connection(),
-        "document": { "name": name, "version": version, "recipes": documents }
+        "document": { "token": applicationToken, "version": version, "recipes": documents }
       },
       { headers: headers });
   }
 
-  updateOneDocument(url: string, token: string, name: string, version: number, documents: Recipe[]) {
+  updateOneDocument(url: string, token: string, applicationToken: string, version: number, documents: Recipe[]) {
     const headers = this.authHeader(token)
       .set('Content-Type', 'application/ejson')
       .set('Accept', 'application/json');
     return this.http.post(url,
       {
         ...this.connection(),
-        "filter": { "name": name },
+        "filter": { "token": applicationToken },
 
         "update": {
           "$set": {
