db6db29fc1e60abb11a208bd6e82f1457e10d674
Dominik Ślusarz <slusarz.dominik@gmail.com>
2024-01-06 17:26:30 +0100

    RM-6 migration mapper

---
+++ src/app/services/recipes/migration-mapper.ts
@@ -0,0 +1,19 @@
+import { Product, Recipe } from "../../recipe";
+
+export class MigrationMapper {
+
+  static migrate(recipes: Recipe[]) {
+    recipes.forEach(r => this.migrateRecipe(r));
+    return recipes;
+  }
+
+  private static migrateRecipe(recipe: Recipe) {
+    recipe.products.forEach(p => this.migrateProduct(p));
+  }
+
+  private static migrateProduct(product: Product) {
+    if (product.scalable === undefined) {
+      product.scalable = true;
+    }
+  }
+}
